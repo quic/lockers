efa519f8f1c7b05fecf94e0c917e4875477add30
Martin Fick <mfick@quicinc.com>
2013-11-04 17:07:01 -0700

    lock_local: convert to use check_lock
    Since most of the code from here was abstracted out into
    check_lock, use check_lock internally.
    
    Change-Id: If74c13a733c70f0c6ed2dd58cd6413023304dfbe
    Signed-off-by: Martin Fick <mfick@quicinc.com>

--- lock_local.sh
+++ lock_local.sh
@@ -3,115 +3,32 @@
 # Copyright (c) 2013, Qualcomm Innovation Center, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 
-#
-# Design:
-#
-# This lock uses a fast_lock and can do run checking to recover stale ids
-# using local_id.sh
-#
-
-q() { "$@" 2>/dev/null ; } # execute a cmd and quiet the stderr
-d() { [ "$DEBUG" = "DEBUG" ] && { echo "$(date) " ; } } # > date(debug) | nothing
-debug() { [ "$DEBUG" = "DEBUG" ] && echo "$(d)$@" >&2 ; }
-info() { debug "$@" ; [ "$DEBUG" = "INFO" ] && echo "$(d)$@" >&2 ; }
-
-args() { # action needed optional [args]...
-    local func=$1 needed=$2 optional=$3 n s min=0 supplied=0 ; shift 3
-    for n in $needed ; do  min=$((min+1)) ;  done
-    for s in "$@" ; do supplied=$((supplied +1)) ; done
-
-    [ $supplied -ge $min ] && return
-    usage "'$func' takes <$needed> [$optional], given ($*)"
-}
-
-is_running() {  # pid|uid
-    [ -z "$1" ] && return 1
-    [ -n "$(pid "$1")" ]
-}
-
 uid() { "${ID_HELPER[@]}" uid "$1" ; } # pid|uid > uid
-
-pid() { # pid|uid > pid (or if helper, blank if uid not running)
-    local pid=$("${ID_HELPER[@]}" pid "$1")
-    local uid=$(uid "$pid")
-    [ "$uid" = "$1" ] && echo "$pid"
-}
-
-clean_stale() { # lock
-    local lock=$1 uid stale=''
-    for uid in $("${FAST_LOCKER[@]}" ids_in_use "$lock") ; do
-         [ -n "$uid" ] || continue
-         is_running "$uid" && continue
-         stale="$stale $uid"
-    done
-    [ -n "$stale" ] && "${FAST_LOCKER[@]}" stale_ids "$lock" $stale
-}
-
-clean_stale_owner() { # lock
-    local lock=$1
-    local owner=$("${FAST_LOCKER[@]}" owner "$lock")
-    [ -z "$owner" ] && return
-
-    local id=$(pid "$owner")
-    [ -n "$id" ] && return
-
-    info "recovery needed for $owner"
-    "${FAST_LOCKER[@]}" stale_ids "$lock" "$owner"
-}
-
-# ---------- API ------------
-
-fast_lock() { # lock pid # => 10 critical error (stop spinning!)
-    local lock=$1 uid=$(uid "$2")
-    "${FAST_LOCKER[@]}" lock "$lock" "$uid"
-}
-
-lock() { # lock pid # => 10 critical error (stop spinning!)
-    args lock "lock pid" "" "$@"
-    local lock=$1 pid=$2  rtn
-
-    clean_stale_owner "$lock"
-    fast_lock "$lock" "$pid" ; rtn=$?
-    return $rtn
-}
-
-unlock() { # lock pid|uid
-    args unlock "lock pid|uid" "" "$@"
-    local lock=$1 uid=$(uid "$2")
-
-    "${FAST_LOCKER[@]}" unlock "$lock" "$uid"
-
-    clean_stale "$lock" &
-}
-
-owner() { # lock > pid
-    args owner "lock" "" "$@"
-    pid "$("${FAST_LOCKER[@]}" owner "$@")"
-}
-
-is_mine() { # lock pid|uid
-    args is_mine "lock pid|uid" "" "$@"
-    local lock=$1 uid=$(uid "$2")
-    [ -z "$uid" ] && return 1
-    "${FAST_LOCKER[@]}" is_mine "$lock" "$uid"
-}
+pid() { "${ID_HELPER[@]}" pid "$1" ; } # pid|uid > pid
 
 usage() { # error_message
     local prog=$(basename "$0")
     cat <<EOF
 
-    usage: $prog lock <lock_path> <pid>
+    usage: $prog -u|-h|--help
+           $prog lock <lock_path> <pid>
+           $prog lock_nocheck <lock_path> <pid>
+           $prog fast_lock <lock_path> <pid>  (DEPRECATED, use lock_nocheck)
            $prog unlock <lock_path> <pid|uid>
 
-           $prog fast_lock <lock_path> <pid>
-           $prog owner <lock_path> > pid
+           $prog owner <lock_path> > pid      (DEPRECATED, see warning)
+           $prog owner_pid <lock_path> > pid
+           $prog owner_pid_nocheck <lock_path> > pid
            $prog is_mine <lock_path> <pid|uid>
+
+    options: -di|--info | -d|--debug
 
     A filesystem based lock manager requiring a pid representing
     the lock holder.
 
-    WARNING: use of owner is discouraged since it returns a
-             potentially non unique pid
+    WARNING: use of owner is going to change, it will soon return
+             uid instead of pid.  If you need a pid (warning:  pids
+             are non-unique over time) use owner_pid instead.
 
 EOF
     [ $# -gt 0 ] && echo "Error - $@" >&2
@@ -122,16 +39,36 @@ mypath=$(readlink -e "$0")
 mydir=$(dirname "$mypath")
 
 ID_HELPER=("$mydir/local_id.sh")
-FAST_LOCKER=("$mydir/fast_lock.sh")
+STALE_CHECKER=("$mydir/local_id.sh" -s is_stale)
+
 while [ $# -gt 0 ] ; do
     case "$1" in
         -u|-h|--help) usage ;;
         -di|--info) DEBUG=INFO ;;
-        -d|--debug) DEBUG=DEBUG ; FAST_LOCKER=("$mydir/fast_lock.sh" --debug) ;;
+        -d|--debug) DEBUG=DEBUG ; STALE_CHECKER+=(-r --debug) ;;
 
         *) break ;;
     esac
     shift
 done
 
-"$@"
+action=$1
+lock=$2
+shift 2
+
+LOCKER=("$mydir"/check_lock.sh $DEBUG)
+CLOCKER=("${LOCKER[@]}" "${STALE_CHECKER[@]}")
+
+case "$action" in
+    owner|owner_pid) pid "$("${CLOCKER[@]}" "owner" "$lock")" ;;
+    owner_pid_nocheck) pid "$("${LOCKER[@]}" "owner_nocheck" "$lock")" ;;
+
+    lock|unlock) "${CLOCKER[@]}" "$action" "$lock" "$(uid "$1")" ;;
+
+    lock_nocheck|fast_lock|is_mine)
+        [ "$action" = "fast_lock" ] && action=lock_nocheck
+        "${LOCKER[@]}" "$action" "$lock" "$(uid "$1")"
+    ;;
+
+    *) usage "unknown action ($action)" ;;
+esac
