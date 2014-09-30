67e31bec2615b141860fdedc6593e01f874999b4
Martin Fick <mfick@quicinc.com>
2014-09-30 10:49:50 -0600

    lockers/test/package: make invocations consistent
    Change-Id: I0c51e0d472380c683a38d52a1b3149062005f2d3
    Signed-off-by: Martin Fick <mfick@quicinc.com>

--- test/package.sh
+++ test/package.sh
@@ -22,8 +22,6 @@ test_file lock_local.sh
 test_file grace_lock.sh
 test_file lock_grace_checker.sh
 test_file ssh_id.sh
-
-echo ; echo "---- Test lock_ssh ----"
-"$MYDIR/lock_ssh.sh" || RESULT=$?
+test_file lock_ssh.sh
 
 exit $RESULT
